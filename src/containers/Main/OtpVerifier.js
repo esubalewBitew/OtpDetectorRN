54e1fe38532ee9fb4762b001486e8db7df222bef
esuye <esubalewbitew13@gmail.com>
2024-01-25 09:38:16 +0300

    created otpVerifid page

---
+++ src/containers/Main/OtpVerifier.js
@@ -0,0 +1,10 @@
+// OtpVerifier.js
+const verifyOtp = (otp) => {
+    // Implement your OTP verification logic here
+    // For simplicity, let's assume a hardcoded OTP for demonstration purposes
+    const hardcodedOtp = '123456';
+
+    return otp === hardcodedOtp;
+};
+
+export default verifyOtp;
